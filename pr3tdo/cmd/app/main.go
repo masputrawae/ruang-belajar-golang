c3df35a2ecead21be9765aa53b4c8fcd45902856
masputrawae <masputrawae@proton.me>
2026-01-29 14:57:20 +0700

    update

---
+++ pr3tdo/cmd/app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"pr3tdo/view/pages"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		pages.Home("Home", "Ini Deskripsi").Render(r.Context(), w)
+	})
+
+	router.Get("/about", func(w http.ResponseWriter, r *http.Request) {
+		pages.About("About", "Ini Deskripsi Di About").Render(r.Context(), w)
+	})
+
+	fmt.Println("Server running: http://localhost:8080")
+	http.ListenAndServe(":8080", router)
+}
---
+++ pr3tdo/go.mod
@@ -0,0 +1,8 @@
+module pr3tdo
+
+go 1.25.5
+
+require (
+	github.com/a-h/templ v0.3.977
+	github.com/go-chi/chi/v5 v5.2.4
+)
