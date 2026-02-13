ac9e56f857439a088d5f0de401b563bf648f25a6
masputrawae <masputrawae@proton.me>
2026-02-14 03:10:35 +0700

    reset

--- pr1idk/pkg/model/model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-// Todo model
-type Todo struct {
-	ID   int    `json:"id"`
-	Task string `json:"task"`
-	Done bool   `json:"done"`
-}
--- pr2idk/sql_template.sql
+++
@@ -1,9 +0,0 @@
-USE belajar;
-CREATE TABLE users (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
-DESC users;
